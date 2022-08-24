636576b4d4417a3f5a999c351b63653ba59ec46e
barnik109 <maitybarnik@gmail.com>
2022-08-25 05:07:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const helmet=require('helmet')
+const cors=require('cors')
+const xss=require('xss-clean')
+const rateLimiter=require('express-rate-limit')
+
+const express = require('express');
+const app = express();
+
+const authRouter=require('./routes/auth')
+const jobsRouter=require('./routes/jobs')
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const authenticateUser=require('./middleware/authentication')
+
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, 
+    max: 100,
+  })
+)
+app.use(express.json());
+// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+
+const connectDB= require('./db/connect')
+
+// routes
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/jobs',authenticateUser,jobsRouter)
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
